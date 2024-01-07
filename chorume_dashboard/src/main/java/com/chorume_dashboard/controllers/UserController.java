c579d68535c3cdea50151bd849a2bc9d0f21580d
devcael <caelgoncalves@icloud.com>
2024-01-07 00:03:22 -0300

    feat: adding frontend project

--- chorume_dashboard/src/main/java/com/chorume_dashboard/controllers/UserController.java
+++ chorume_dashboard/src/main/java/com/chorume_dashboard/controllers/UserController.java
@@ -1,5 +1,9 @@
 package com.chorume_dashboard.controllers;
 
+import com.chorume_dashboard.repositorys.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,11 +14,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class UserController {
+  @Autowired
+  UserRepository userRepository;
 
   @GetMapping("/users")
   @ResponseBody
-  public String index() {
-    return "users/index";
+  public ResponseEntity<?> index() {
+    return new ResponseEntity<>(userRepository.findAll(), HttpStatus.OK);
   }
 
 }
